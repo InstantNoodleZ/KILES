c4bc5a1db443912dffe36d755af3add640210b46
Richard Berger <bergerrichard16@gmail.com>
2018-04-14 15:05:40 +0200

    ver 0.2 - rand geht und autofocus

--- JS - PROJECT - Tiere erraten/tierspiel.js
+++ JS - PROJECT - Tiere erraten/tierspiel.js
@@ -1,25 +1,9 @@
-//var pics = ["hund", "katze", "panda", "adler", "flamingo", "goldfisch", "schildkroete", "schnecke", "waschbaer", "wellensittich", 
-  //                  "loewe", "chamaeleon", "delfin", "tintenfisch", "eichhoernchen", "tiger", "gorilla", "faultier"];
-var pics = ["hund", "katze", "panda"];
+var pics = ["hund", "katze", "panda", "adler", "flamingo", "goldfisch", "schildkroete", "schnecke", "waschbaer", "wellensittich", 
+            "loewe", "chamaeleon", "delfin", "tintenfisch", "eichhoernchen", "tiger", "gorilla", "faultier"];
 
 function rando() {
     var abfrage = Math.floor(Math.random() * pics.length);
-        if(pics[abfrage] != "leer") {
-            return abfrage;
-        } 
-        else if(pics.every(leer)) {
-            document.write("gschofft :D looooool");
-        }
-        else {
-            rando();
-            
-        }
-}
-
-
-function leer(tier) {
-    return tier = "leer";
-}
+    return abfrage; }
 
 var rand = rando();
 
@@ -32,12 +16,18 @@ function myFunction() {
     
         if(x[0].value==pics[rand]) {
             document.getElementById("tierinput").value = "";
-            document.getElementById("tierinput").focus;                 //funzt nu ned
+            document.getElementById("eingabe").focus;                 //funzt nu ned
             document.getElementById("result").style.color = "green";    //grüner Text wenns richtig is
             text = "des woa richtig ;-)";
             pics[rand] = "leer";
             rand = rando();
-            randTier(); }
+            
+            while(pics[rand] == "leer") {
+                rand = rando(); } 
+            
+            randTier(); 
+            document.getElementById("tierinput").focus(); }
+        
     
         else {
             document.getElementById("result").style.color = "red";      //roter Text wenns falsch is
