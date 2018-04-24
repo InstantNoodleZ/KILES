9d46da28917edc807a42b04402469d825836a8d0
Simon Stockinger <32122290+InstantNoodleZ@users.noreply.github.com>
2018-04-24 15:08:15 +0200

    Tierspiel "vollständig" - Soos
    Saas

--- JS - PROJECT - Tiere erraten/tierspiel.js
+++ JS - PROJECT - Tiere erraten/tierspiel.js
@@ -1,12 +1,26 @@
-var pics = ["Hund", "Katze", "Panda", "Adler"];//, "Flamingo", "Goldfisch", "Schildkröte", "Schnecke", "Waschbär", "Wellensittich", 
-            //"Löwe", "Chamäleon", "Delfin", "Tintenfisch", "Eichhörnchen", "Tiger", "Gorilla", "Faultier"];
+var pics = ["Hund", "Katze", "Panda", "Adler", "Flamingo", "Goldfisch", "Schildkröte", "Schnecke", "Waschbär", "Wellensittich", 
+            "Löwe", "Chamäleon", "Delfin", "Tintenfisch", "Eichhörnchen", "Tiger", "Gorilla", "Faultier"];
 
 function rando() {
     var abfrage = Math.floor(Math.random() * pics.length);
-    return abfrage; }
+    return abfrage;
+}
 
 var rand = rando();
+var str = "Dein Punktestand:&nbsp;";
 var points = 0;
+
+
+var skpp    =   -50;  //skip penalty
+var succ    =   100;  //success rate
+var flsp    =   -10;  //false penalty
+
+var skpmsg  =   "Bild &uuml;bersrpungen!";
+var skpclr  =   "blue";
+var sucmsg  =   "des woa richtig ;-)";
+var succlr  =   "green";
+var faimsg  =   "leider foisch :-(";
+var faiclr  =   "red";
 
 document.getElementById("tierinput").onkeypress = function(event){
     if (event.keyCode == 13 || event.which == 13){
@@ -14,8 +28,38 @@ document.getElementById("tierinput").onkeypress = function(event){
     }
 };
 
+function skip() {
+    document.getElementById("tierinput").value = "";            
+    document.getElementById("result").style.color = skpclr;
+    text = skpmsg + "<br>Das Tier war ein " + pics[rand];
+    points += skpp;
+    pics[rand] = "leer";
+    rand = rando();
+    for(var i = 0; i < pics.length; i++) {
+        if (pics[i] != "leer") {
+            while(pics[rand] == "leer") {
+                rand = rando(); }
+        }
+    }
+    randTier();
+    document.getElementById("tierinput").focus();
+    document.getElementById("points").innerHTML = str + points;
+    document.getElementById("result").innerHTML = text;
+}
+
 function randTier() {
-        document.getElementById("bild").src = "images/"+pics[rand]+".jpg"; } 
+        if( pics[rand] == "leer" )
+            parseToScreen();
+        document.getElementById("bild").src = "images/"+pics[rand]+".jpg";
+}
+
+function parseToScreen() {
+    sessionStorage.setItem("points", points);
+    window.open("tierspiel_win.html",'_self',false); 
+}
+function loadScore() {
+    document.getElementById("win_points").innerHTML = str + sessionStorage.getItem("points");
+}
       
 function myFunction() {
     var text = "";
@@ -23,25 +67,30 @@ function myFunction() {
     
         if(x[0].value==pics[rand]) {
             document.getElementById("tierinput").value = "";            
-            document.getElementById("result").style.color = "green";    //grüner Text wenns richtig is
-            text = "des woa richtig ;-)";
+            document.getElementById("result").style.color = succlr;    //grüner Text wenns richtig is
+            text = sucmsg;
             pics[rand] = "leer";
             rand = rando();
             
-            while(pics[rand] == "leer") {
-                rand = rando(); } 
+            for(var i = 0; i < pics.length; i++) {
+                if (pics[i] != "leer") {
+                    while(pics[rand] == "leer") {
+                        rand = rando(); }
+                }
+            }
             
             randTier();
             document.getElementById("tierinput").focus();
-            points += 100;
-            document.getElementById("points").innerHTML = points;
+            points += succ;
+            document.getElementById("points").innerHTML = str + points;
         }
     
         else {
-            document.getElementById("result").style.color = "red";      //roter Text wenns falsch is
-            text = "leider foisch :-(";
-            points -= 10;
-            document.getElementById("points").innerHTML = points;
+            document.getElementById("result").style.color = faiclr;      //roter Text wenns falsch is
+            text = faimsg;
+            points += flsp;
+            document.getElementById("points").innerHTML = str + points;
         } 
     
-    document.getElementById("result").innerHTML = text; }
+    document.getElementById("result").innerHTML = text;
+}
