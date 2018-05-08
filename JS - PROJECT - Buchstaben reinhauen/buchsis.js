504a680637eb83968917ffa7fe0d1707ac93e5e1
Simon Stockinger <32122290+InstantNoodleZ@users.noreply.github.com>
2018-05-08 14:30:32 +0200

    saaas
    fick di

--- JS - PROJECT - Buchstaben reinhauen/buchsis.js
+++ JS - PROJECT - Buchstaben reinhauen/buchsis.js
@@ -1,25 +1,27 @@
-var letters = ['f', 't', 'ß', 'ä'] ;//, 'ä', 'e', 'z', 'c', 'g', 't', 'l', 'm', 'z', 'p', 't', 'v', 't', 'p', 'k', 'g', 'e', 'ß', 'G'];
+var letters = ['f', 't', 'ß', 'ä']; //, 'ä', 'e', 'z', 'c', 'g', 't', 'l', 'm', 'z', 'p', 't', 'v', 't', 'p', 'k', 'g', 'e', 'ß', 'G'];
 
 
 function check() {
-    
+
     var fail = 0;
 
-    for (var i = 1; i < 24; i++) {
+    for (var i = 0; i < letters.length; i++) {
         //if(document.getElementById(i).value != letters[i-1])   {
         var indiv = document.getElementById(i);
-        if (indiv.getElementsByTagName("input")[0].value != letters[i - 1] || indiv.getElementsByTagName("input")[0].value == "") {
+        document.getElementById("richardisatrottl").innerHTML = indiv.getElementsByTagName("input")[0].value + "&nbsp;" + letters[i];
+        if (indiv.getElementsByTagName("input")[0].value != letters[i] || indiv.getElementsByTagName("input")[0].value == "") {
             fail++;
         } else {
-            document.getElementById(i).innerHTML = letters[i - 1];
+            document.getElementById(i).innerHTML = letters[i];
             document.getElementById(i).style.backgroundColor = "green";
         }
     }
-    if (fail == 0) {
+    
+    /*if (fail == 0) {
         document.getElementById("richardisatrottl").innerHTML = "kan föhla :D";
     } else {
-        document.getElementById("richardisatrottl").innerHTML = "12";
-    }
+        document.getElementById("richardisatrottl").innerHTML = fail;
+    }*/
 }
 
 //document.getElementById("myBtn").disabled = true;
